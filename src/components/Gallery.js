8420242381e4062b8215266e7426c7e869822bc5
JayeshNakum04 <jbnakum04@gmail.com>
2025-02-15 20:43:19 +0530

    Added new image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,7 +6,7 @@ const Gallery = () => {
     <section id="gallery">
       <h2>Photo Gallery</h2>
       <div className="gallery-grid">
-       <img src="/images/Placeholder 1.jpg" alt="My Image" />
+       <img src="public\images\Placeholder 1.jpg" alt="My Image" />
 
       </div>
     </section>
