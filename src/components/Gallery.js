a1930ceb1adbcf67e36546860fd5c13d5658a391
JayeshNakum04 <jbnakum04@gmail.com>
2025-02-15 21:59:48 +0530

    Updates

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,7 +6,10 @@ const Gallery = () => {
     <section id="gallery">
       <h2>Photo Gallery</h2>
       <div className="gallery-grid">
-      <img src="/images/my1.jpg" alt="My Image" />
+      <img src={`${process.env.PUBLIC_URL}/images/my3.jpg`} alt="Image 1" />
+      <img src={`${process.env.PUBLIC_URL}/images/my4.jpg`} alt="Image 2" />
+      <img src={`${process.env.PUBLIC_URL}/images/my7.jpg`} alt="Image 3" />
+      
 
       </div>
     </section>
